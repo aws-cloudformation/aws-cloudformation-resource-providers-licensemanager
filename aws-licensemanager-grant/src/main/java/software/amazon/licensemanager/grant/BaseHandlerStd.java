bb8d0450346476f4ed383f3d9ceae081e7c1de04
Mandy Zhao <zhaomand@amazon.com>
2020-11-22 02:41:14 -0800

    Fix style

--- aws-licensemanager-grant/src/main/java/software/amazon/licensemanager/grant/BaseHandlerStd.java
+++ aws-licensemanager-grant/src/main/java/software/amazon/licensemanager/grant/BaseHandlerStd.java
@@ -10,25 +10,25 @@ import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 // Placeholder for the functionality that could be shared across Create/Read/Update/Delete/List Handlers
 
 public abstract class BaseHandlerStd extends BaseHandler<CallbackContext> {
-  @Override
-  public final ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-          final AmazonWebServicesClientProxy proxy,
-          final ResourceHandlerRequest<ResourceModel> request,
-          final CallbackContext callbackContext,
-          final Logger logger) {
+    @Override
+    public final ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final Logger logger) {
     return handleRequest(
-            proxy,
-            request,
-            callbackContext != null ? callbackContext : new CallbackContext(),
-            proxy.newProxy(ClientBuilder::getClient),
-            logger
+        proxy,
+        request,
+        callbackContext != null ? callbackContext : new CallbackContext(),
+        proxy.newProxy(ClientBuilder::getClient),
+        logger
     );
   }
 
-  protected abstract ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-          final AmazonWebServicesClientProxy proxy,
-          final ResourceHandlerRequest<ResourceModel> request,
-          final CallbackContext callbackContext,
-          final ProxyClient<LicenseManagerClient> proxyClient,
-          final Logger logger);
+    protected abstract ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final ProxyClient<LicenseManagerClient> proxyClient,
+        final Logger logger);
 }
--- aws-licensemanager-license/src/main/java/software/amazon/licensemanager/license/CreateHandler.java
+++ aws-licensemanager-license/src/main/java/software/amazon/licensemanager/license/CreateHandler.java
@@ -129,4 +129,3 @@ public class CreateHandler extends BaseHandlerStd {
     }
 
 }
-
